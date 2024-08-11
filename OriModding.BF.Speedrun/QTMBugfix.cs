9d1ecff6369748c4c7070d01423370d66ee2bbc0
Kirefel <kirefelan@gmail.com>
2024-08-11 11:08:14 +1000

    Copy qol

---
+++ OriModding.BF.Speedrun/QTMBugfix.cs
@@ -0,0 +1,28 @@
+﻿namespace KFT.OriBF.Qol;
+
+internal static class QTMBugfix
+{
+    public static bool delayNavigation;
+
+    public static void Init()
+    {
+        On.CleverMenuItemSelectionManager.Start += (orig, self) =>
+        {
+            delayNavigation = Core.Input.MenuDown.IsPressed || Core.Input.MenuUp.IsPressed;
+            orig(self);
+        };
+
+        On.CleverMenuItemSelectionManager.FixedUpdate += (orig, self) =>
+        {
+            if (self.IsActive && delayNavigation && !Core.Input.MenuDown.IsPressed && !Core.Input.MenuUp.IsPressed)
+                delayNavigation = false;
+            orig(self);
+        };
+
+        On.CleverMenuItemSelectionManager.MoveSelection += (orig, self, forward) =>
+        {
+            if (!delayNavigation)
+                orig(self, forward);
+        };
+    }
+}
