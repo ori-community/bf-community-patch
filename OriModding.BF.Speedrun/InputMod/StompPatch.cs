f3ceeca503e47f50614a65e1ad817d7313c1ce09
Kirefel <kirefelan@gmail.com>
2025-07-22 09:18:01 +1000

    Fix stomp

--- OriModding.BF.Speedrun/InputMod/StompPatch.cs
+++ OriModding.BF.Speedrun/InputMod/StompPatch.cs
@@ -1,20 +1,20 @@
-using HarmonyLib;
-using OriModding.BF.Core;
-using OriModding.BF.InputLib;
-
-namespace OriModding.BF.Speedrun;
-
-[HarmonyPatch(typeof(SeinStomp), nameof(SeinStomp.UpdateStompInactiveState))]
-internal class StompPatch
-{
-    private static bool Prefix(SeinStomp __instance)
-    {
-        if (__instance.Sein.Controller.IsSwimming)
-            return HarmonyHelper.StopExecution;
-
-        // if (CustomInput.Stomp.OnPressed && !CustomInput.Stomp.Used && __instance.CanStomp())
-        //     __instance.Logic.ChangeState(__instance.State.StompIdle);
-            
-        return HarmonyHelper.StopExecution;
-    }
-}
+// using HarmonyLib;
+// using OriModding.BF.Core;
+// using OriModding.BF.InputLib;
+//
+// namespace OriModding.BF.Speedrun;
+//
+// [HarmonyPatch(typeof(SeinStomp), nameof(SeinStomp.UpdateStompInactiveState))]
+// internal class StompPatch
+// {
+//     private static bool Prefix(SeinStomp __instance)
+//     {
+//         if (__instance.Sein.Controller.IsSwimming)
+//             return HarmonyHelper.StopExecution;
+//
+//         // if (CustomInput.Stomp.OnPressed && !CustomInput.Stomp.Used && __instance.CanStomp())
+//         //     __instance.Logic.ChangeState(__instance.State.StompIdle);
+//             
+//         return HarmonyHelper.StopExecution;
+//     }
+// }
